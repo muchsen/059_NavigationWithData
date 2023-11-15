fcbdf817ed5fc0a56c40b49569424149d4583771
TA1059 <muchsen.ft21@mail.umy.ac.id>
2023-11-15 16:43:48 +0700

    Buat Val Halaman

--- app/src/main/java/com/example/esjumbo/data/OrderUIState.kt
+++ app/src/main/java/com/example/esjumbo/data/OrderUIState.kt
@@ -4,5 +4,8 @@ data class OrderUIState(
     val  jumlah: Int = 0,
     val rasa: String = "",
     val harga: String = "",
+    val namaPelanggan: String = "",
+    val nomorTelepon: String = "",
+    val alamat: String = ""
 
 )
